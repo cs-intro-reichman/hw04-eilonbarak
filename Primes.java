412f1bed1a99dcbe08a8fe024cd9e51d284565f3
Eilon Barak <eilon.barak@post.runi.ac.il>
2025-11-26 16:23:40 +0200

    done 1/3

--- Primes.java
+++ Primes.java
@@ -3,11 +3,33 @@ public class Primes {
 
         int number = Integer.parseInt(args[0]);
         boolean[] isPrime = new boolean[number + 1];
+        int count = 0;
+
+        for (int i = 2; i <= number; i++) {// איתחול כל המספרים ל-true
+            isPrime[i] = true;
+
+        }
+
+        for (int i = 2; i * i <= number; i++) {// לולאה ראשית מ-2 עד שורש n
+            if (isPrime[i]) {
+
+                for (int j = i * 2; j <= number; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+
+        }
 
         for (int i = 2; i <= number; i++) {
-            isPrime[i] = true; // מניחים שכולם ראשוניים
+            if (isPrime[i]) {
+                System.out.println(i);
+                count++;
 
-            
+            }
         }
+        double percent = (double) count / (number - 1) * 100;
+
+        System.out.println("There are " + count + " primes between 2 and " + number + " (" + percent + "% are primes)");
+
     }
 }
